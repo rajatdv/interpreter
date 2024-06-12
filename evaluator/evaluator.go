cba0afc62cd7a154af11fc880ddf76f95d10e07d
Rajat Agarwal <rajatagarwal699@gmail.com>
2024-06-13 02:25:04 +0530

    Added evaluator and test

---
+++ evaluator/evaluator.go
@@ -0,0 +1,46 @@
+package evaluator
+
+import (
+	"interpreter/ast"
+	"interpreter/object"
+)
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	//Statement
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	//Expression
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
