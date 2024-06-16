7fb736ca10db1f7cb6c2b5aa616e4c332662f3ed
Rajat Agarwal <rajatagarwal699@gmail.com>
2024-06-16 15:00:34 +0530

    Added array data type

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -16,6 +16,7 @@ func TestNextToken(t *testing.T) {
 	5 < 10 > 5 ;
 	"foobar"
 	"foo bar"
+	[1, 2];
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -71,6 +72,12 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
